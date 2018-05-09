42827a897cd39aabaeefe3d694afd47a87d52276
MelissaMB <meli.gaby0895@gmail.com>
2018-05-09 15:13:09 -0600

    Creación de Activity Asignatura

---
+++ app/src/main/java/com/example/oscar/reservalab/Asignatura.java
@@ -0,0 +1,8 @@
+package com.example.oscar.reservalab;
+
+/**
+ * Created by Melissa on 09/05/2018.
+ */
+
+public class Asignatura {
+}
--- app/src/main/java/com/example/oscar/reservalab/MainActivity.java
+++ app/src/main/java/com/example/oscar/reservalab/MainActivity.java
@@ -1,15 +1,42 @@
 package com.example.oscar.reservalab;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+
+    String[] menu={"Tabla Asignatura", "LLenar Base de Datos"};
+    String[] activities={"AsignaturaMenuActivity"};
 
 
-//Prueba de commit...
-public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menu));
+    }
+
+    @Override
+    protected void onListItemClick(ListView l,View v,int position,long id){
+        super.onListItemClick(l, v, position, id);
+        if(position!=3){
+            String nombreValue=activities[position];
+            try{
+                Class<?> clase=Class.forName("com.example.oscar.reservalab."+nombreValue);
+                Intent inte = new Intent(this,clase);
+                this.startActivity(inte);
+
+            }catch(ClassNotFoundException e){
+                e.printStackTrace();
+            }
+        }else{
+//CODIGO PARA LLENAR BASE DE DATOS
+        }
     }
 }
